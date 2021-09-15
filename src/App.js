774eaffa99692a23bd4c511e21461736b174d7cc
Matija Karapetrić <matija@epicentar.net>
2021-09-15 19:26:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
+import logo from './log.svg';
 import './App.css';
+import * as dayjs from "dayjs";
+import { nanoid } from 'nanoid';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Pozdrav</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
+        <p>Ovo je React aplikacija koja prikazuje trenutni datum i ID korisnika.</p>
+        <p>Today is {dayjs().format('MMMM DD, YYYY HH:mm:ss')}</p>
+        <p>Your unique ID is {nanoid(8)}</p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://moj-posao.hr"
           target="_blank"
           rel="noopener noreferrer"
         >
